9e62ea735bec024d6edb947a27247a17d33ca300
Dorian URBANIAK <dorian.urbaniak@gmail.com>
2022-11-15 11:28:30 +0100

    git init

---
+++ src/pages/EditRecipe/EditRecipe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditRecipe = () => {
+  return (
+    <div className="main-container">
+      <h1>edit recipe</h1>
+    </div>
+  );
+};
+
+export default EditRecipe;
---
+++ src/pages/RecipeDetails/RecipeDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecipeDetails = () => {
+  return (
+    <div className="main-container">
+      <h1>recipe details</h1>
+    </div>
+  );
+};
+
+export default RecipeDetails;
