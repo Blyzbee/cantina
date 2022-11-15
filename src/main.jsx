9bae241e00f5d37eb4a9a844f28026ce8e88d8bf
Dorian URBANIAK <dorian.urbaniak@gmail.com>
2022-11-15 11:58:49 +0100

    scoped sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import AddNewRecipe from "./pages/AddNewRecipe/AddNewRecipe";
+import EditRecipe from "./pages/EditRecipe/EditRecipe";
+import RecipeDetails from "./pages/RecipeDetails/RecipeDetails";
+import RecipeList from "./pages/RecipeList/RecipeList";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <RecipeList />,
+  },
+  {
+    path: "/recette/:id",
+    element: <RecipeDetails />,
+  },
+  {
+    path: "/modifier-recette/:id",
+    element: <EditRecipe />,
+  },
+  {
+    path: "/ajouter-recette/:id",
+    element: <AddNewRecipe />,
   },
 ]);
 
